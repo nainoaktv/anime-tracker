805c323c5d85c34136f7d3f705a9a834fd4b8e78
nainoktv <nainoaktvillegas@gmail.com>
2022-09-17 14:39:38 -0700

    chore: add anime model and migration file

---
+++ models/anime.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class anime extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  anime.init({
+    title: DataTypes.STRING,
+    episodes: DataTypes.INTEGER,
+    image_url: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'anime',
+  });
+  return anime;
+};
